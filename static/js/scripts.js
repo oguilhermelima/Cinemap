02d036b748dc8de9b3577d386c5f79d091e3dc59
Guilherme Lima <guilherme.oliveira95@fatec.sp.gov.br>
2018-06-09 03:54:16 -0300

    Mudança header

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -406,3 +406,47 @@ function enableSwiper(){
 		
 	}
 }
+
+	$('#parallax').mousemove(
+		function(e){
+			/* Detecta a posição do mouse */
+			var offset = $(this).offset();
+			var xPos = e.pageX - offset.left;
+			var yPos = e.pageY - offset.top;
+
+			/* Pega a posição em porcentagem */
+			var mouseXPercent = Math.round(xPos / $(this).width() * 100);
+			var mouseYPercent = Math.round(yPos / $(this).height() * 100);
+
+			/* Posiciona cada camada */
+			$(this).children('img').each(
+				function(){
+					var diffX = $('#parallax').width() - $(this).width();
+					var diffY = $('#parallax').height() - $(this).height();
+
+					var myX = diffX * (mouseXPercent / 100); //) / 100) / 2;
+
+					var myY = diffY * (mouseYPercent / 100);
+
+					var cssObj = {
+						'left': myX + 'px',
+						'top': myY + 'px'
+					}
+					//$(this).css(cssObj);
+					$(this).animate({
+						left: myX, 
+						top: myY
+					},
+					{
+						duration: 50, 
+						queue: false, 
+						easing: 'linear'
+					});
+
+				}
+			);
+
+		}
+	);
+
+var event = ('ontouchstart' in window) ? 'click' : 'mouseenter mouseleave';
